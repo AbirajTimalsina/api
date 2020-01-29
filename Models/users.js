a139dbfad0c271791ec3b74ea1165032e0d569fc
Abiraj Timalsina <timalsinaabiraj92@gmail.com>
2020-01-29 12:16:34 +0545

    backend running

---
+++ Models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 6
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
